c178a1bff49480fa163a15a5e5ed75e7ddf2030e
Stenson Morgan English <stensonmorganenglish@Stensons-MacBook-Pro.local>
2016-11-07 00:43:53 -0500

    3rd commit - finished looking through Kristyn's closet array

--- unit_1/w1d5/homework/solutions.js
+++ unit_1/w1d5/homework/solutions.js
@@ -23,3 +23,47 @@ function median(values) {
 median(nums);
 
 // commit "2nd commit - finished Find the Median array question "
+
+var kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+// Thom's closet is more complicated. Check out this nested data structure!!
+var thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+
+console.log("Kristyn is rocking that " + kristynsCloset[2] + "today!");
+
+var kristynShoe = kristynsCloset[0];
+console.log (kristynShoe);
+
+
+kristynsCloset.splice((kristynsCloset.length - 1), 0, "raybans" );
+console.log(kristynsCloset);
+
+kristynsCloset[5] = 'stained knit hat';
+console.log(kristynsCloset);
+
+// 3rd commit "3rd commit - finished looking through Kristyn's closet array"
